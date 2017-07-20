2e355057c1650afb85f0a1ab49fece0989815d8b
David Kokkelink <david.kokkelink@gmail.com>
2017-07-20 11:02:39 +0200

    fix translation scope

--- lib/utils/add_editor_nav.php
+++ lib/utils/add_editor_nav.php
@@ -14,10 +14,10 @@ function add_editor_nav(&$page, $step, $type = "chart") {
         $page['createstep'] = $step;
     } else if ($type == "map") {
         $steps = array();
-        $steps[] = array('index'=>1, 'id'=>'basemap', 'title'=>__('step-1', 'd3-maps'));
-        $steps[] = array('index'=>2, 'id'=>'data', 'title'=>__('step-2', 'd3-maps'));
-        $steps[] = array('index'=>3, 'id'=>'visualize', 'title'=>__('step-3', 'd3-maps'));
-        $steps[] = array('index'=>4, 'id'=>'publish', 'title'=>__('step-4', 'd3-maps'));
+        $steps[] = array('index'=>1, 'id'=>'basemap', 'title'=>__('step-1', 'd3-maps-ui'));
+        $steps[] = array('index'=>2, 'id'=>'data', 'title'=>__('step-2', 'd3-maps-ui'));
+        $steps[] = array('index'=>3, 'id'=>'visualize', 'title'=>__('step-3', 'd3-maps-ui'));
+        $steps[] = array('index'=>4, 'id'=>'publish', 'title'=>__('step-4', 'd3-maps-ui'));
         $page['steps'] = $steps;
         $page['chartLocale'] = $page['locale'];
         $page['metricPrefix'] = get_metric_prefix($page['chartLocale']);
